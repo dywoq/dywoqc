b623de58c7d637219bcd93b15d4a6a49498e6feb
dywoq <aleks.koyf@gmail.com>
2025-06-03 23:06:25 +0300

    [global] rebuild everything

--- include/dywoqc/error.h
+++
@@ -1,9 +0,0 @@
-#ifndef DYWOQC_ERROR_H
-#define DYWOQC_ERROR_H
-
-// IWYU pragma: begin_keep
-#include "__error/fatal.h"
-#include "__error/struct.h"
-// IWYU pragma: end_keep
-
-#endif
--- include/dywoqc/pair.h
+++
@@ -1,9 +0,0 @@
-#ifndef DYWOQC_PAIR_H
-#define DYWOQC_PAIR_H
-
-// IWYU pragma: begin_keep
-#include "__pair/anonymous.h"
-#include "__pair/typedef.h"
-// IWYU pragma: end_keep
-
-#endif
